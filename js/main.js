83aac0d0451f5781db1a66f840cef564bfc67551
Alex Cviek <a.i.cwiek@gmail.com>
2017-04-04 16:46:56 +0100

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// card values
+
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
