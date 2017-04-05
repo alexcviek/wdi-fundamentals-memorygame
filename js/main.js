715bd7df6ace05eda1173c70c2ee6230653c22fe
Alex Cviek <a.i.cwiek@gmail.com>
2017-04-05 21:47:00 +0100

    created 2 functions and cleaned up the code

--- js/main.js
+++ js/main.js
@@ -1,19 +1,9 @@
 // array of cards
 var cards = ['queen', 'queen', 'king', 'king'];
-
+//array of cards in play
 var cardsInPlay = [];
-
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if(cardsInPlay.length === 2){
+//checking if both cards selected are the same
+var checkForMatch = function(){
 	if(cardsInPlay[0] === cardsInPlay[1]){
 		alert('You found a match!');
 	}
@@ -21,6 +11,20 @@ if(cardsInPlay.length === 2){
 		alert('Sorry, try again :(');
 	}
 }
-else{
+//when user flips the card
+var flipCard = function(cardId){
 
+console.log("User flipped " + cards[cardId]);
+//card goes to our cards in play array
+cardsInPlay.push(cards[cardId]);
+//check if user has chosen 2 cards
+if(cardsInPlay.length === 2){
+	checkForMatch();
 }
+	else{
+		//to be completed!
+	}
+}
+
+flipCard(0);
+flipCard(2);
