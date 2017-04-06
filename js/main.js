8c8b62240026af25f0cd92340b9ca4668a35e104
Alex Cviek <a.i.cwiek@gmail.com>
2017-04-06 12:58:50 +0100

    created objects

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
-// array of cards
-var cards = ['queen', 'queen', 'king', 'king'];
+// array of card objects
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
+];
 //array of cards in play
 var cardsInPlay = [];
 //checking if both cards selected are the same
@@ -14,9 +35,11 @@ var checkForMatch = function(){
 //when user flips the card
 var flipCard = function(cardId){
 
-console.log("User flipped " + cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+console.log(cards[cardId].suit);
+console.log(cards[cardId].cardImage);
 //card goes to our cards in play array
-cardsInPlay.push(cards[cardId]);
+cardsInPlay.push(cards[cardId].rank);
 //check if user has chosen 2 cards
 if(cardsInPlay.length === 2){
 	checkForMatch();
