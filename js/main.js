c51910d9c0b3343b15aae61ccf07f054af071476
Alex Cviek <a.i.cwiek@gmail.com>
2017-04-07 10:49:15 +0100

    final verion, bonuses to be added

--- js/main.js
+++ js/main.js
@@ -21,8 +21,10 @@ var cards = [
 		cardImage: "images/king-of-diamonds.png"
 	},
 ];
+
 //array of cards in play
 var cardsInPlay = [];
+
 //checking if both cards selected are the same
 var checkForMatch = function(){
 	if(cardsInPlay[0] === cardsInPlay[1]){
@@ -33,13 +35,18 @@ var checkForMatch = function(){
 	}
 }
 //when user flips the card
-var flipCard = function(cardId){
+var flipCard = function(){
+var cardId = this.getAttribute('data-id');
+
 
 console.log("User flipped " + cards[cardId].rank);
 console.log(cards[cardId].suit);
 console.log(cards[cardId].cardImage);
 //card goes to our cards in play array
 cardsInPlay.push(cards[cardId].rank);
+
+this.setAttribute('src', cards[cardId].cardImage);
+
 //check if user has chosen 2 cards
 if(cardsInPlay.length === 2){
 	checkForMatch();
@@ -49,5 +56,14 @@ if(cardsInPlay.length === 2){
 	}
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', [i]);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
